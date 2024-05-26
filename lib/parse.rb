be2f75b145a29176538d598c65578d952e013d9f
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-05-26 15:24:43 -0300

    wip grupamento

--- lib/parse.rb
+++ lib/parse.rb
@@ -13,9 +13,12 @@ module B3ExcelParse
     QTD = 'Quantidade'
     UNIT_PRICE = 'Preço unitário'
     TOTAL_PRICE = 'Valor da Operação'
-    # Bonificação em Ativo we cannot calculate because the company give the value and does not shows in .xlsx
-    WARN_KEYWORDS = ['Fração em Ativos', 'Empréstimo', 'Bonificação em Ativos'].freeze
-
+    # Na Bonificação em Ativo realmente não conseguimos calcular, porque o valor é baseado no valor dito pela empresa e não temos esse valor no excel
+    # No desdobramento de ações a quantidade é a quantidade de ativos novos (Como se fosse uma operação de compra)
+    # No grupamento de ações a quantidade é a quantidade final de ações após o grupoamento
+    #   - Caso tenha um número fracionário: Em sequêcia teremos 2 eventos Leilão de fração e Fração em Ativos 
+    WARN_KEYWORDS = ['Fração em Ativos', 'Fração em Ativos', 'Empréstimo', 'Bonificação em Ativos'].freeze
+    # TODO: Handle Grupamento, Leilão de Fração 
 
     def initialize(files_path)
       @rows = []
@@ -64,18 +67,27 @@ module B3ExcelParse
       product_name.split[0]
     end
 
+    # Informações de compra
+    #   Para o cálculo segundo as fontes não precisamos levar em consideração as vendas
+    #
+    #   Fonte:
+    #     - https://ajuda.nuinvest.com.br/hc/pt-br/articles/360049317813-Como-realizar-o-c%C3%A1lculo-do-Pre%C3%A7o-M%C3%A9dio
     # @return [total_bought_price, total_bought_amount]
     def bought_info(transactions)
       total_price = 0
       amount = 0
       transactions.each do |t|
         price = 0
+        # TODO: Reduzir quantidade de items
         if t[TYPE] == 'Transferência - Liquidação' && t[SIDE] == 'Credito'
           price = t[TOTAL_PRICE]
           amount += t[QTD].to_i
         end
         if t[TYPE] == 'Desdobro'
           amount +=  t[QTD].to_i
+        end
+        if t[TYPE] == 'Grupamento'
+          amount = t[QTD].to_i # Podemos ignorar as frações já que serão posteriormente leiloadas
         end
         price = 0 if price.is_a?(String)
         total_price += price
@@ -86,7 +98,8 @@ module B3ExcelParse
     def amount_to_sum(row)
       return row[QTD].to_i if row[TYPE] == 'Transferência - Liquidação' && row[SIDE] == 'Credito'
       return -row[QTD].to_i if row[TYPE] == 'Transferência - Liquidação' && row[SIDE] == 'Debito'
-      return row[QTD].to_i if row[TYPE] == 'Desdobro'
+      return row[QTD].to_f if row[TYPE] == 'Desdobro'
+      return -row[QTD].to_f if row[TYPE] == 'Fração em Ativos' && row[SIDE] == 'Debito'
 
       0
     end
